a5834437c7f0a8df034f9d2e7d0e0a652610f28d
nakamura196 <na.kamura.1263@gmail.com>
2021-05-03 06:45:27 +0900

    update to shoho-yashima

--- functions/index.js
+++ functions/index.js
@@ -3,20 +3,16 @@ const functions = require("firebase-functions");
 // Expressの読み込み
 const express = require("express");
 const cors = require('cors')
+const fs = require("fs");
+const convert = require('xml-js');
 
 const app = express();
 app.use(cors())
 
-//公文書館のデータをまとめたもの
-const config = require('./config.json')
+const host = "https://nakamura196.github.io/dzi"
 
 app.get('/', (_req, res) => {
-  const url =
-    _req.protocol + '://' + _req.get('host') + "/api" + _req.originalUrl + '/info.json'
-  res.writeHead(302, {
-    Location: url,
-  })
-  res.end()
+  return res.json({"hello": "world"})
 })
 
 app.get('/iiif-img/:id', (_req, res) => {
@@ -29,18 +25,18 @@ app.get('/iiif-img/:id', (_req, res) => {
 })
 
 app.get('/iiif-img/:id/info.json', (_req, res) => {
-  const id = _req.params.id
-  
-  //設定ファイルに含まれていない場合
-  if (!config[id]) {
-    return res.json({})
-  }
+  const params = _req.params
+  const id = params.id
 
-  const obj = config[id]
+  const data = fs.readFileSync('./data/'+id+'.dzi','utf8')
+  const config = JSON.parse(convert.xml2json(data, options))
 
-  const width = obj.width
-  const height = obj.height
-  const tile = obj.tile
+  const image = config.elements[0]
+  const sizeInfo = image.elements[0].attributes
+
+  const imageWidth = Number(sizeInfo.Width)
+  const imageHeight = Number(sizeInfo.Height)
+  const tile = Number(image.attributes.TileSize)
 
   //サイズやタイルの部分は改善の余地あり
   const info = {
@@ -50,7 +46,7 @@ app.get('/iiif-img/:id/info.json', (_req, res) => {
       '://' +
       _req.get('host') + "/api" + 
       _req.originalUrl.replace('/info.json', ''),
-    height,
+    height: imageHeight,
     profile: [
       'http://iiif.io/api/image/2/level0.json',
       {
@@ -59,6 +55,7 @@ app.get('/iiif-img/:id/info.json', (_req, res) => {
       },
     ],
     protocol: 'http://iiif.io/api/image',
+    /*
     sizes: [
       {
         height: 99,
@@ -93,6 +90,7 @@ app.get('/iiif-img/:id/info.json', (_req, res) => {
         width: 1,
       },
     ],
+    */
     tiles: [
       {
         height: tile,
@@ -100,41 +98,40 @@ app.get('/iiif-img/:id/info.json', (_req, res) => {
         width: tile,
       },
     ],
-    width,
+    width: imageWidth,
   }
 
   return res.json(info)
 })
 
-app.get('/iiif-img/:id/:region/:size/:rotaion/default.jpg', (_req, res) => {
+app.get('/iiif-img2/:id/:region/:size/:rotaion/default.jpg', (_req, res) => {
   const params = _req.params
   const id = params.id
   const region = params.region
   const size = params.size
 
-  //設定ファイルに含まれていない場合
-  if (!config[id]) {
-    return res.json({})
-  }
+  const data = fs.readFileSync('./data/'+id+'.dzi','utf8')
+  const config = JSON.parse(convert.xml2json(data, { ignoreComment: true, alwaysChildren: true }))
 
-  const obj = config[id]
+  const image = config.elements[0]
+  const sizeInfo = image.elements[0].attributes
 
-  const imageWidth = obj.width
-  const imageHeight = obj.height
-  const tile = obj.tile
+  const imageWidth = Number(sizeInfo.Width)
+  const imageHeight = Number(sizeInfo.Height)
+  const tile = Number(image.attributes.TileSize)
 
   const longDimension = Math.max(imageWidth, imageHeight)
   const maxLevel = parseInt(Math.ceil(Math.log2(longDimension)))
 
-  const prefix = obj.dzi.replace('.json', '')
+  const prefix = host + "/" + id + "/" + id + "_files/" //obj.dzi.replace('.json', '')
 
   let url = ''
 
   if (region === 'full' && size === 'full') {
-    url = 'https://www.digital.archives.go.jp/das/image-l/' + id
+    url = prefix + "0/0_0.jpg"
   } else if (region === 'full') {
     const t = Math.ceil(Math.log2(longDimension / tile))
-    url = prefix + '_files/' + t + '/0_0.jpg'
+    url = prefix + t + '/0_0.jpg'
   } else {
     const xywh = region.split(',')
     const x = Number(xywh[0])
@@ -161,7 +158,7 @@ app.get('/iiif-img/:id/:region/:size/:rotaion/default.jpg', (_req, res) => {
     const x0 = Math.ceil(x / window)
     const y0 = Math.ceil(y / window)
 
-    url = prefix + '_files/' + level + '/' + x0 + '_' + y0 + '.jpg'
+    url = prefix + level + '/' + x0 + '_' + y0 + '.jpg'
   }
 
   res.writeHead(302, {
@@ -170,11 +167,13 @@ app.get('/iiif-img/:id/:region/:size/:rotaion/default.jpg', (_req, res) => {
   res.end()
 })
 
+/*
 const port = '3001'
 app.listen(port, () => {
   // eslint-disable-next-line
   console.log(`app start listening on port ${port}`)
 })
+*/
 
 // 出力
 const api = functions.https.onRequest(app);
